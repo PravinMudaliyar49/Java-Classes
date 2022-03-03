d61a080ad0b4c90d7000976efaf324b6585be0f2
Pravin <pravincoding49@gmail.com>
2022-03-03 20:10:15 +0530

    Update

--- isAnagram.java
+++ isAnagram.java
@@ -5,13 +5,16 @@ public class isAnagram {
         String s1 = "adcc";
         String s2 = "abdc";
 
-        boolean ans = isAnagramUnoptimised(s1, s2);
+        // boolean ans = isAnagramUnoptimised(s1, s2);
+        boolean ans = isAnagramOptimised(s1, s2);
         System.out.println(ans);
 
     }
 
     public static boolean isAnagramUnoptimised(String s1, String s2) {
-        if(s1.length() != s2.length()){
+        //TIME: 0(N^N) and Space: O(N).
+
+        if (s1.length() != s2.length()) {
             return false;
         }
 
@@ -24,8 +27,8 @@ public class isAnagram {
             for (int j = 0; j < s2.length(); j++) {
                 char ch2 = s2.charAt(j);
 
-                if(check[j] == false){
-                    if(ch1 == ch2){
+                if (check[j] == false) {
+                    if (ch1 == ch2) {
                         chFound = true;
                         check[j] = true;
                         break;
@@ -34,7 +37,38 @@ public class isAnagram {
 
             }
 
-            if(chFound == false){
+            if (chFound == false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isAnagramOptimised(String s1, String s2) {
+        //TIME: 0(N) and Space: O(1).
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        int[] s1arr = new int[26];
+        int[] s2arr = new int[26];
+
+        //Bharne ka kaam
+        for (int i = 0; i < s1.length(); i++) {
+            char ch1 = s1.charAt(i);
+            char ch2 = s2.charAt(i);
+
+            int asc1 = ch1;
+            int asc2 = ch2;
+
+            s1arr[asc1 - 97]++;
+            s2arr[asc2 - 97]++;
+        }
+
+        //Check karne ka kaam.
+        for(int i=0; i<s1.length(); i++){
+            if(s1arr[i] != s2arr[i]){
                 return false;
             }
         }
