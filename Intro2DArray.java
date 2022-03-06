8b846f14bd7cc0e6bbde90e0962a0de5327af70c
Pravin <pravincoding49@gmail.com>
2022-03-06 14:32:02 +0530

    End of 6th March

---
+++ Intro2DArray.java
@@ -0,0 +1,16 @@
+public class Intro2DArray {
+    public static void main(String[] args) {
+        int[][] arr = new int[2][3];
+
+        arr[0][0] = 1;
+        arr[0][1] = 2;
+        arr[0][2] = 3;
+        arr[1][0] = 4;
+        arr[1][1] = 5;
+        arr[1][2] = 6;
+
+        System.out.println(arr.length);
+        System.out.println(arr[0].length);
+
+    }
+}
