0eb0659d5c85c2b934ba68bfb8abb806f3a01409
Pravin <pravincoding49@gmail.com>
2022-03-07 20:49:02 +0530

    Matrix multiplication input

---
+++ matrixMultiplication.java
@@ -0,0 +1,16 @@
+public class matrixMultiplication {
+    public static void main(String[] args) {
+        int[][] input1 = {
+                { 10, 0, 2 },
+                { 0, 3, 0 }
+        };
+
+        int[][] input2 = {
+                { 1, 2, 0, 1 },
+                { 0, 1, 0, 0 },
+                { 0, 0, 3, 0 }
+        };
+
+        
+    }
+}
