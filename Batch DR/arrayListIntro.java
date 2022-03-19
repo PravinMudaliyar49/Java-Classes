6532befa19ee4c2c7cc223e072f0e510905f8ab4
Pravin <pravincoding49@gmail.com>
2022-03-19 17:35:32 +0530

    git ignore dummy

---
+++ Batch DR/arrayListIntro.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class arrayListIntro {
+    public static void main(String[] args) throws Exception {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // int i = 10;
+        // list.add(i);
+
+        // list.add(20);
+        // list.add(30);
+        // list.add(40);
+        // list.add(50);
+
+        // list.add(1, 100);
+
+        // System.out.println(list + " -> " + list.size());
+
+        // list.remove(2);
+
+        // System.out.println(list + " -> " + list.size());
+
+        // int eleAtInd = list.get(2);
+        // System.out.println(eleAtInd);
+
+        // list.set(2, 100);
+
+        // System.out.println(list + " -> " + list.size());
+
+        // for (int j = 0; j < list.size(); j++) {
+        // System.out.print(list.get(j) + " ");
+        // }
+
+        // int[] arr = {100, 200, 300, 400 ,500};
+
+        // int idx = 0;
+        // for(int val : list){
+        // // System.out.print(val + " ");
+        // System.out.print(list.get(idx) + " ");
+
+        // idx++;
+        // }
+
+        // System.out.println();
+
+        // for(int val : arr){
+        // System.out.print(val + " ");
+        // }
+
+        // InputStreamReader ir = new InputStreamReader(System.in);
+        // BufferedReader br = new BufferedReader(ir);
+
+        // String str = br.readLine();
+        // System.out.println(str);
+
+        // long val = Long.parseLong(br.readLine());
+        // System.out.println(val * 100);
+
+    }
+}
