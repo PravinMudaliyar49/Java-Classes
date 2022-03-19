ed1a4b0ff3f5c2d5cf828481d3c497fba769f909
Pravin <pravincoding49@gmail.com>
2022-03-20 00:42:13 +0530

    Completed ArrayList theory and for-each

--- Batch DR/arrayListIntro.java
+++ Batch DR/arrayListIntro.java
@@ -1,61 +1,57 @@
-import java.util.*;
+// import java.util.*;
 import java.io.*;
 
 public class arrayListIntro {
     public static void main(String[] args) throws Exception {
-        ArrayList<Integer> list = new ArrayList<>();
+        // ArrayList<Integer> list = new ArrayList<>();
 
-        // int i = 10;
-        // list.add(i);
-
+        // list.add(10);
         // list.add(20);
         // list.add(30);
         // list.add(40);
         // list.add(50);
 
-        // list.add(1, 100);
-
-        // System.out.println(list + " -> " + list.size());
+        // list.set(2, 100);
 
         // list.remove(2);
 
         // System.out.println(list + " -> " + list.size());
 
-        // int eleAtInd = list.get(2);
-        // System.out.println(eleAtInd);
+        // System.out.println(list.get(2));
 
-        // list.set(2, 100);
-
-        // System.out.println(list + " -> " + list.size());
-
-        // for (int j = 0; j < list.size(); j++) {
-        // System.out.print(list.get(j) + " ");
-        // }
-
-        // int[] arr = {100, 200, 300, 400 ,500};
-
-        // int idx = 0;
-        // for(int val : list){
-        // // System.out.print(val + " ");
-        // System.out.print(list.get(idx) + " ");
-
-        // idx++;
+        // for (int i = 0; i < list.size(); i++) {
+            // System.out.print(list.get(i) + " ");
         // }
 
         // System.out.println();
 
+        
+        // for(int val : list){
+            // System.out.print(val + " ");
+        // }
+        
+        // int[] arr = {10, 20, 30, 40, 50, 60};
+
         // for(int val : arr){
-        // System.out.print(val + " ");
+        //     System.out.print(val + " ");
         // }
 
-        // InputStreamReader ir = new InputStreamReader(System.in);
-        // BufferedReader br = new BufferedReader(ir);
+        // String str = "Hello world";
 
-        // String str = br.readLine();
-        // System.out.println(str);
+        // for(char ch : str.toCharArray()){
+        //     System.out.print(ch + " ");
+        // }
 
-        // long val = Long.parseLong(br.readLine());
-        // System.out.println(val * 100);
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+
+        String str = br.readLine();
+
+        int i = Integer.parseInt(br.readLine());
+
+        // "235" -> 235.
+
+        System.out.println(str);
 
     }
 }
