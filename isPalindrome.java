2f78ae84c24c593463a0f07e9b6f3d9e303096b7
Pravin <pravincoding49@gmail.com>
2022-03-20 12:25:43 +0530

    Completed isPalindrome

--- isPalindrome.java
+++ isPalindrome.java
@@ -1,22 +1,20 @@
-import java.util.Scanner;
-
 public class isPalindrome {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        String str = "nitin";
+        boolean ans = isPalindromeCheck(str);
 
-        while(true){
-            int n = sc.nextInt();
-    
-            boolean ans = isPrimeOptimised(n);
-            System.out.println(ans);
-        }
+        System.out.println(ans);
     }
 
-    public static boolean isPrimeOptimised(int n) {
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
+    public static boolean isPalindromeCheck(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i <= j) {
+            if (str.charAt(i) != str.charAt(j)) {
                 return false;
             }
+
+            i++;
+            j--;
         }
 
         return true;
---
+++ isPrimeOptimised.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class isPrimeOptimised {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            int n = sc.nextInt();
+    
+            boolean ans = isPrimeOptimised(n);
+            System.out.println(ans);
+        }
+    }
+
+    public static boolean isPrimeOptimised(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
