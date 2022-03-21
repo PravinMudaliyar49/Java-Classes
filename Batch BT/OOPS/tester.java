0d9c34b1fa583c57b45c49916a823b696c583a53
Pravin <pravincoding49@gmail.com>
2022-03-21 21:32:49 +0530

    Programs based on classes and objects

--- Batch BT/OOPS/tester.java
+++ Batch BT/OOPS/tester.java
@@ -2,6 +2,52 @@ package OOPS;
 
 public class tester {
     public static void main(String[] args) {
-        
+        Human bhavesh = new Human();
+        bhavesh.age = 19;
+        bhavesh.hasTwoEyes = true;
+        bhavesh.sirname = "Kodam";
+
+        bhavesh.talk();
+
+        Person p = new Person();
+        p.name = "Bhavesh";
+        p.age = 19;
+        p.saysHi();
+
+        Person p2 = new Person();
+        p2.name = "Tarun";
+        p2.age = 19;
+        p2.saysHi();
+
+        p.saysHi();
     }
+}
+
+class Human {
+    int age;
+    boolean hasTwoEyes;
+    String sirname;
+
+    void run() {
+
+    }
+
+    void jump() {
+
+    }
+
+    void talk() {
+        System.out.println("Somthing random talks");
+    }
+
+}
+
+class Person{
+    String name;
+    int age;
+
+    void saysHi(){
+        System.out.println(name + " of this age : " + age + " says hii.");
+    }
+
 }
