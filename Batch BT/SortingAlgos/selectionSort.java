dbc33e33ec69976b39950542331ab746a70e42bc
Pravin <pravincoding49@gmail.com>
2022-03-22 21:59:25 +0530

    Completed selection sort

---
+++ Batch BT/SortingAlgos/selectionSort.java
@@ -0,0 +1,26 @@
+// package SortingAlgos;
+
+import java.util.*;
+
+public class selectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 10, 19, 6, 7, 21, 34 };
+
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
