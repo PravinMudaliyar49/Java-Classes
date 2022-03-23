bd9ef1aa6e3439eae495aef85d141138b0be029c
Pravin <pravincoding49@gmail.com>
2022-03-24 02:08:27 +0530

    Basics of Classes and objects

--- Batch DR/OOPS/tester.java
+++ Batch DR/OOPS/tester.java
@@ -1,13 +1,27 @@
 package OOPS;
 
+import java.util.Scanner;
+
 public class tester {
     public static void main(String[] args) {
-        Human man = new Human();
-        man.age = 20;
-        man.name = "Random";
+        Scanner sc = new Scanner(System.in);
 
-        man.speak();
-        man.run();
+        // man.age = 20;
+        // man.name = "Random";
+        
+        // man.needs(sc.nextLine(), "Football");
+        
+        // man.speak();
+        // man.run();
+        
+        Human man1 = new Human();
+        man1.age = 20;
+        man1.name = "Random";
+        man1.introduceYourself();
+
+        Human man2 = new Human();
+        man2.introduceYourself();
+
     }
 }
 
@@ -15,11 +29,19 @@ class Human {
     int age;
     String name;
 
+    void introduceYourself(){
+        System.out.println("My name is " + name + ". And my age is " + age + ".");
+    }
+
     void run() {
         System.out.println("I am running");
     }
 
     void speak() {
         System.out.println("I am talking");
+    }
+
+    void needs(String food, String game){
+        System.out.println("Humans needs are " + food + " and " + game);
     }
 }
