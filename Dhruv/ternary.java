426014ff5fe4f559c9a6d150d423a3580c66f030
Pravin <pravincoding49@gmail.com>
2022-04-08 17:20:37 +0530

    Completed Palindrome number and ternary operator

---
+++ Dhruv/ternary.java
@@ -0,0 +1,29 @@
+public class ternary {
+    public static void main(String[] args) {
+        int a = 100, b = 30, c = 200, d = 0;
+
+        // if(a > b){
+        // c = a;
+        // }else{
+        // c = b;
+        // }
+
+        // c = a > b ? b : a;
+
+        if (a > b) {
+            if (a > c) {
+                d = a;
+            } else {
+                d = c;
+            }
+        } else if (b > c) {
+            d = b;
+        } else {
+            d = c;
+        }
+
+        d = (a > b ? (a > c ? a : c) : (b > c ? b : c));
+
+        System.out.println(d);
+    }
+}
